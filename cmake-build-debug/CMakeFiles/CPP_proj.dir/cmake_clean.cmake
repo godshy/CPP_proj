bfe93c13ecc7851f38d85e25fb8c6109a675e290
shenhongyi <asse9653@hotmail.com>
2020-11-02 13:30:01 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CPP_proj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_proj.dir/list_/1.cpp.o"
+  "CMakeFiles/CPP_proj.dir/main.cpp.o"
+  "CPP_proj"
+  "CPP_proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
